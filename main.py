316af129fb78f49c7ffdffa98a61a71c7bb1f81c
Zaed Khan <zaedkhan2019@gmail.com>
2024-01-29 10:42:44 +0600

    Accessed news titles and descriptions from API

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import requests
 api_key = "34bebf4dc50745fd811a8f4acf5abff8"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-12-29&sortBy=publishedAt&apiKey=34bebf4dc50745fd811a8f4acf5abff8"
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with dara
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
